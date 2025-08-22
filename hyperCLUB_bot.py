a2f41b7411d488fd8c8bb1b6868b6c2295e375d4
Arjun T M <56509491+cyberkallan@users.noreply.github.com>
2025-08-22 14:25:20 +0530

    hyperCLUB_bot.py

--- hyperCLUB_bot.py
+++ hyperCLUB_bot.py
@@ -13,7 +13,7 @@ def _xor_decode(s):
 def _split_decode(parts):
     return ''.join(_xor_decode(p) for p in parts)
 
-# Obfuscated sensitive data
+# Sensitive data (do not edit)
 _obf_data = {
     'url': ['aHR0cHM6Ly9uZW9jYXQuc3RvcmUvd2Vic2l0ZS9hcGkvdmFsaWRhdGVfbGljZW5zZS5waHA='],
     'api_base': ['aHR0cHM6Ly93d3cuY2x1YmhvdXNlYXBpLmNvbS9hcGkv'],
@@ -446,3 +446,4 @@ if __name__ == "__main__":
     bot.send_message(launch_msg)
     
     bot.run()
+
